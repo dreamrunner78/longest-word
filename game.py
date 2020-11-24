f002fc0221276dcf7de15c4b0fb2c6318026a6f6
EL Baroudi Ahmed Bassim <bassim.elbaroudi@gmail.com>
2020-11-24 16:11:15 +0100

    Feature complete: Dictionary check of attempt

--- game.py
+++ game.py
@@ -1,4 +1,5 @@
 import string, random
+import requests
 
 class Game ():
 
@@ -10,7 +11,14 @@ class Game ():
 
 
     def is_valid (self, word):
-        res = [c in self.grid for c in word.upper()]
-        if False in res:
-            return False
-        return True
+        #res = [c in self.grid for c in word.upper()]
+        #if False in res:
+        #    return False
+        #return True
+        self.__check_dictionary(word)
+
+    @staticmethod
+    def __check_dictionary(word):
+        response = requests.get(f"https://wagon-dictionary.herokuapp.com/{word}")
+        json_response = response.json()
+        return json_response['found']
